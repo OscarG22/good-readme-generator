eb9492281c5b6103d5d50cacf364ccb4ab4c80e7
Oscar Grajeda <oscargrajeda2@gmail.com>
2020-10-14 20:47:18 -0700

    Added function to initialize program. Added questions to array.

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,8 @@
+const fs = require('fs');
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
 const inquirer = require("inquirer");
-
-
-
-
+const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // array of questions for user
@@ -15,22 +15,58 @@ const questions = [{
     type: "input",
     message: "Enter a description of your project.",
     name: "description"
-}
-];
+},
+{
+    type: "input",
+    message: "Enter installation instructions for your project.",
+    name: "installation"
+},
+{
+    type: "input",
+    message: "",
+    name: "usage"
+},
+{
+    type: "input",
+    message: "Select your license of choice.",
+    name: "license"
+},
+{
+    type: "input",
+    message: "Did anyone else contribute to this project?",
+    name: "contribution"
+},
+{
+    type: "input",
+    message: "Please provide test-instructions.",
+    name: "test-instructions"
+},
+{
+    type: "input",
+    message: "Enter your GitHub username.",
+    name: "questions"
+},
+{
+    type: "input",
+    message: "Enter your email address.",
+    name: "questions"
+}];
+//
+
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(fileName, newRead) {
+    writeFileAsync('README.MD', newReadMe)
 }
+// function writeToFile(fileName, data) {
+//     writeFileAsync('README.MD', newReadMe)
+// }
 
 // function to initialize program
 function init() {
     inquirer.prompt(questions).then(function(answers){
-        const markdown = `
-        ## QUESTIONS
-        ${answers.questions}
-        
-        ## LICENSE
-        `
+        let markdown = generateMarkdown(answers)
+        console.log(markdown)
     })
 }
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,11 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  #[![License: FDL 1.3](https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)](https://www.gnu.org/licenses/fdl-1.3)
 
+  //#endregion
+
+  #
 `;
 }
 
